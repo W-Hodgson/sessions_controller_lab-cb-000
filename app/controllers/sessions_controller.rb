40e1dfbb699c7344654d794c6a74b66b407177d5
Will Hodgson <will.hodgson@current-rms.com>
2019-11-18 06:44:42 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if !session[:name] || session[:name].empty?
+      return redirect_to(controller: 'sessions', action: 'new')
+    end
   end
 
   def destroy
