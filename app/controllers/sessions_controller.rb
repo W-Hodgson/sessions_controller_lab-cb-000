dad9459b3276ea3c02971e0d05413871f120d3b3
Will Hodgson <will.hodgson@current-rms.com>
2019-11-18 07:03:56 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if !session[:name] || session[:name].empty?
-      return redirect_to(controller: 'sessions', action: 'new')
-    end
-
+    return redirect_to(controller: 'sessions',
+                       action: 'new') if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
-    redirect_to(controller: 'application', action: 'hello')
+    redirect_to controller: 'application', action: 'hello'
   end
 
   def destroy
